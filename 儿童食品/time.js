4dbd59d833503ce8910f158e03b098b1c52b5d5a
Yukiiyeye <119338328+Yukiiyeye@users.noreply.github.com>
2022-12-06 22:26:08 +0800

    Update time.js

--- 儿童食品/time.js
+++ 儿童食品/time.js
@@ -1,4 +1,13 @@
-
+function showTime(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    document.getElementById("time").innerHTML = year+"年"+month+"月"+day+"日"+hour+"时"+minute+"分"+second+"秒";
+}
 setInterval("showTime()",1000);
 // Path: 儿童食品\index.js
 // 轮播图
